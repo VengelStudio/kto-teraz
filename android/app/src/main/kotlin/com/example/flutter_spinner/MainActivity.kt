003757a3428045293ddaa8eaa6af0bf82c293ea7
Bartosz Kepka <bartoszkepka1999@gmail.com>
2021-02-28 18:35:12 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_spinner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_spinner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kto teraz?',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue,
+          disabledColor: Colors.grey,
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          splashColor: Colors.blueAccent,
+          textTheme: ButtonTextTheme.primary,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+Widget _buildPopupDialog(BuildContext context, String text) {
+  return new AlertDialog(
+    title: const Text('Popup'),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(text),
+      ],
+    ),
+    actions: <Widget>[
+      new FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(bottom: 40),
+              child: Text(
+                'Kto teraz?',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      _buildPopupDialog(context, 'Testowy text zeby zobaczyc'),
+                );
+              },
+              child: Text(
+                "Zagraj",
+                style: TextStyle(fontSize: 30.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
