151bd286f75e81fe0e5d136b432ce5e0b9d80e0f
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-05-03 18:44:56 +0200

    Add loading questions from JSON

--- lib/views/game_page.dart
+++ lib/views/game_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:flutter_spinner/utils/emojis.utils.dart';
 import 'package:flutter_spinner/utils/winner.model.dart';
@@ -39,9 +42,55 @@ class PlayerSlice {
   }
 }
 
+Future<List<Question>> loadQuestions(BuildContext context) async {
+  final data = await DefaultAssetBundle.of(context)
+      .loadString('assets/json/questions.json');
+
+  return compute(parseQuestions, data);
+}
+
+// A function that converts a response body into a List<Photo>.
+List<Question> parseQuestions(String rawJson) {
+  final parsed = jsonDecode(rawJson);
+
+  return parsed.map<Question>((json) => Question.fromJson(json)).toList();
+}
+
+class Question {
+  final String text;
+  final double probability;
+  final bool isTabu;
+
+  Question({this.text, this.probability, this.isTabu});
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      text: json['text'] as String,
+      probability: json['probability'] as double,
+      isTabu: json['isTabu'] as bool,
+    );
+  }
+}
+
 class _GameState extends State<GamePage> {
   int selectedPlayerIndex = 0;
   int nextDurationInS = 4;
+  List<Question> questions = [];
+  int questionIndex = 0;
+
+  _loadQuestions() async {
+    var fetchedQuestions = await loadQuestions(context);
+
+    setState(() {
+      questions = fetchedQuestions;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadQuestions();
+  }
 
   Winner winner = new Winner();
 
@@ -68,9 +117,7 @@ class _GameState extends State<GamePage> {
       });
     }
 
-    // todo temporary dialog, remove this when implementing question popup
     showQuestionCard(BuildContext context) {
-      // show the dialog
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -80,7 +127,6 @@ class _GameState extends State<GamePage> {
     }
 
     openQuestion() {
-      // todo temporary dialog, remove this when implementing question popup
       showQuestionCard(context);
       print(winner);
     }
@@ -90,39 +136,50 @@ class _GameState extends State<GamePage> {
         child: Column(
           children: [
             Expanded(
-                child: Container(
-                    margin: const EdgeInsets.only(left: 10, right: 10),
-                    child: PhysicalShape(
-                      color: Colors.transparent,
-                      shadowColor: Colors.black,
-                      elevation: 8,
-                      clipper: ShapeBorderClipper(shape: CircleBorder()),
-                      child: FortuneWheel(
-                        physics: CircularPanPhysics(
-                          duration: Duration(seconds: nextDurationInS),
-                          curve: Curves.ease,
-                        ),
-                        onFling: spinWheel,
-                        onAnimationEnd: openQuestion,
-                        animateFirst: false,
-                        selected: selectedPlayerIndex,
-                        indicators: <FortuneIndicator>[wheelIndicator],
-                        items: [
-                          for (var player in players)
-                            FortuneItem(
-                              child: Emojis.getTransformedEmoji(player.emoji),
-                              style: FortuneItemStyle(
-                                color: player
-                                    .color, // <-- custom circle slice fill color
-                                borderColor: Colors
-                                    .black38, // <-- custom circle slice stroke color
-                                borderWidth:
-                                    1, // <-- custom circle slice stroke width
-                              ),
+              child: Container(
+                  margin: const EdgeInsets.only(left: 10, right: 10),
+                  child: questions.length > 0
+                      ? PhysicalShape(
+                          color: Colors.transparent,
+                          shadowColor: Colors.black,
+                          elevation: 8,
+                          clipper: ShapeBorderClipper(shape: CircleBorder()),
+                          child: FortuneWheel(
+                            physics: CircularPanPhysics(
+                              duration: Duration(seconds: nextDurationInS),
+                              curve: Curves.ease,
                             ),
-                        ],
-                      ),
-                    ))),
+                            onFling: spinWheel,
+                            onAnimationEnd: openQuestion,
+                            animateFirst: false,
+                            selected: selectedPlayerIndex,
+                            indicators: <FortuneIndicator>[wheelIndicator],
+                            items: [
+                              for (var player in players)
+                                FortuneItem(
+                                  child:
+                                      Emojis.getTransformedEmoji(player.emoji),
+                                  style: FortuneItemStyle(
+                                    color: player
+                                        .color, // <-- custom circle slice fill color
+                                    borderColor: Colors
+                                        .black38, // <-- custom circle slice stroke color
+                                    borderWidth:
+                                        1, // <-- custom circle slice stroke width
+                                  ),
+                                ),
+                            ],
+                          ),
+                        )
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            Text("Ładowanie pytań...")
+                          ],
+                        )),
+            ),
           ],
         ),
       ),
