1302c338f9e900a75b749d04ba35f85059219529
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-05-03 09:35:03 +0200

    Add random colors

--- lib/views/game_page.dart
+++ lib/views/game_page.dart
@@ -8,43 +8,67 @@ class GamePage extends StatefulWidget {
   _GameState createState() => _GameState();
 }
 
+class PlayerSlice {
+  String text;
+
+  Color color;
+
+  PlayerSlice(String text) {
+    this.text = text;
+    this.color = getColor();
+  }
+
+  static Random random = new Random();
+
+  static Color getColor() {
+    return Color.fromARGB(
+        255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+  }
+}
+
 class _GameState extends State<GamePage> {
   int selected = 0;
+  int nextDurationInS = 4;
 
   @override
   Widget build(BuildContext context) {
-    final items = <String>[
-      'Grogu',
-      'Mace Windu',
-      'Obi-Wan Kenobi',
-      'Han Solo',
-      'Luke Skywalker',
-      'Darth Vader',
-      'Yoda',
-      'Ahsoka Tano',
+    final items = <PlayerSlice>[
+      new PlayerSlice("Bartek"),
+      new PlayerSlice("Lukasz"),
+      new PlayerSlice("Bartek"),
+      new PlayerSlice("Lukasz"),
+      new PlayerSlice("Bartek"),
+      new PlayerSlice("Lukasz"),
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Fortune Wheel'),
-      ),
       body: GestureDetector(
         child: Column(
           children: [
             Expanded(
               child: FortuneWheel(
                 physics: CircularPanPhysics(
-                  duration: Duration(seconds: 4),
-                  curve: Curves.decelerate,
+                  duration: Duration(seconds: nextDurationInS),
+                  curve: Curves.ease,
                 ),
                 onFling: () {
                   setState(() {
+                    nextDurationInS = Random().nextInt(4) + 1;
                     selected = Random().nextInt(items.length);
                   });
                 },
                 selected: selected,
                 items: [
-                  for (var it in items) FortuneItem(child: Text(it)),
+                  for (var it in items)
+                    FortuneItem(
+                      child: Text(it.text),
+                      style: FortuneItemStyle(
+                        color: it.color, // <-- custom circle slice fill color
+                        borderColor: Colors
+                            .green, // <-- custom circle slice stroke color
+                        borderWidth: 3, // <-- custom circle slice stroke width
+                      ),
+                    ),
                 ],
               ),
             ),
