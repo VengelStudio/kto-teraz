c77d2a2133d41dd181a4d2ff53fdbb1d51bb17f4
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-05-03 10:02:50 +0200

    Add emojis and hue colors

--- lib/views/game_page.dart
+++ lib/views/game_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:flutter_svg/svg.dart';
 
 class GamePage extends StatefulWidget {
   @override
@@ -9,20 +10,48 @@ class GamePage extends StatefulWidget {
 }
 
 class PlayerSlice {
-  String text;
+  int id;
 
   Color color;
 
-  PlayerSlice(String text) {
-    this.text = text;
-    this.color = getColor();
+  SvgPicture emoji;
+
+  PlayerSlice(int id, Color color, SvgPicture emoji) {
+    this.id = id;
+    this.color = color;
+    this.emoji = emoji;
   }
 
-  static Random random = new Random();
+  static getEmojiSvg(int index) {
+    // todo shuffle
 
-  static Color getColor() {
-    return Color.fromARGB(
-        255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    final emojis = [
+      "assets/images/emoji/mouse.svg",
+      "assets/images/emoji/cow.svg",
+      "assets/images/emoji/elephant.svg",
+      "assets/images/emoji/turtle.svg",
+      "assets/images/emoji/chicken.svg",
+      "assets/images/emoji/pinguin.svg",
+      "assets/images/emoji/rabbit.svg",
+      "assets/images/emoji/cat.svg",
+      "assets/images/emoji/monkey.svg",
+      "assets/images/emoji/dog.svg",
+      "assets/images/emoji/pig.svg",
+      "assets/images/emoji/unicorn.svg",
+      "assets/images/emoji/duck.svg",
+      "assets/images/emoji/shark.svg",
+      "assets/images/emoji/dinosaur.svg",
+    ];
+
+    return SvgPicture.asset(emojis[index % emojis.length]);
+  }
+
+  static List<PlayerSlice> generate(int howMany) {
+    List colors = new List.generate(howMany,
+        (i) => HSLColor.fromAHSL(1, 360 * i / howMany, 0.7, 0.5).toColor());
+
+    return new List.generate(
+        howMany, (i) => new PlayerSlice(i, colors[i], getEmojiSvg(i)));
   }
 }
 
@@ -30,16 +59,11 @@ class _GameState extends State<GamePage> {
   int selected = 0;
   int nextDurationInS = 4;
 
+  int temp_player_count = 20;
+
   @override
   Widget build(BuildContext context) {
-    final items = <PlayerSlice>[
-      new PlayerSlice("Bartek"),
-      new PlayerSlice("Lukasz"),
-      new PlayerSlice("Bartek"),
-      new PlayerSlice("Lukasz"),
-      new PlayerSlice("Bartek"),
-      new PlayerSlice("Lukasz"),
-    ];
+    final items = PlayerSlice.generate(temp_player_count);
 
     return Scaffold(
       body: GestureDetector(
@@ -61,7 +85,8 @@ class _GameState extends State<GamePage> {
                 items: [
                   for (var it in items)
                     FortuneItem(
-                      child: Text(it.text),
+                      // child: Text(it.text),
+                      child: it.emoji,
                       style: FortuneItemStyle(
                         color: it.color, // <-- custom circle slice fill color
                         borderColor: Colors
