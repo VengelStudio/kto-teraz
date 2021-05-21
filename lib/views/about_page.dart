a9b4998f26a34bf57e85f21f485127beff541e11
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-05-21 08:02:50 +0200

    Add scrollable list

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -9,58 +9,56 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(bottom: 60),
-              child: Text(
-                'O aplikacji',
-                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-                child: Column(
+        body: SingleChildScrollView(
+            child: Column(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(top: 60, bottom: 30),
+          child: Text(
+            'Kto teraz?',
+            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+            child: Column(
+          children: [
+            Row(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 30.0),
-                      child: Text(
-                        "Poznajcie się w błyskawicznym tempie! ⚡\n\n🌟 Kręcicie kołem fortuny\n🌟 \"Kto teraz?\"\n🌟 Wylosowana osoba odpowiada na pytanie",
-                        style: TextStyle(fontSize: 14),
-                      ),
+                Flexible(
+                    child: Text(
+                  "Poznajcie się w błyskawicznym tempie! ⚡\n\n🌟 Kręcicie kołem fortuny\n🌟 \"Kto teraz?\"\n🌟 Wylosowana osoba odpowiada na pytanie",
+                  style: TextStyle(fontSize: 18),
+                )),
+              ],
+            ),
+            SizedBox(height: 20),
+            new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Text(
+                      "Developerzy",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                     ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 30.0),
-                      child: Text(
-                        "Developerzy",
-                        style: TextStyle(fontSize: 28),
-                      ),
+                  ),
+                  Text(
+                    "Bartosz Kępka",
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  Text(
+                    "Łukasz Blachnicki",
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 5),
+                    child: Text(
+                      "Wsparcie",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                     ),
-                    Text(
-                      "Bartosz Kępka",
-                      style: TextStyle(fontSize: 28),
-                    ),
-                    Text(
-                      "Łukasz Blachnicki",
-                      style: TextStyle(fontSize: 28),
-                    ),
-                  ],
-                ),
-                Text(
-                  "Wsparcie",
-                  style: TextStyle(fontSize: 28),
-                ),
-                SingleChildScrollView(
-                    child: new Column(children: <Widget>[
+                  ),
                   Text(
                     "Zuzanna Olasek",
                     style: TextStyle(fontSize: 28),
@@ -81,40 +79,10 @@ class _AboutPageState extends State<AboutPage> {
                     "Sebastian Kula",
                     style: TextStyle(fontSize: 28),
                   ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                  Text(
-                    "Developerzy",
-                    style: TextStyle(fontSize: 28),
-                  ),
-                ]))
-              ],
-            )),
+                ])
           ],
-        ),
-      ),
-    );
+        )),
+      ],
+    )));
   }
 }
