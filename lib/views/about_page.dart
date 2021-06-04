4dc1a87a78bd2861e6637812b5285e15491305a4
Bartosz Kępka <bartoszkepka1999@gmail.com>
2021-06-04 21:57:29 +0200

    Change style of about page

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -13,74 +13,97 @@ class _AboutPageState extends State<AboutPage> {
             child: Column(
       children: <Widget>[
         Container(
-          margin: const EdgeInsets.only(top: 60, bottom: 30),
-          child: Text(
-            'Kto teraz?',
-            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Container(
             margin: const EdgeInsets.only(left: 30, right: 30),
             child: Column(
               children: [
-                Row(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Flexible(
-                        child: Text(
-                      "Poznajcie się w błyskawicznym tempie! ⚡\n\n🌟 Kręcicie kołem fortuny\n🌟 \"Kto teraz?\"\n🌟 Wylosowana osoba odpowiada na pytanie",
-                      style: TextStyle(fontSize: 18),
-                    )),
+                    SizedBox(height: 40),
+                    Container(
+                      margin: const EdgeInsets.only(top: 60, bottom: 30),
+                      child: Text(
+                        'Kto teraz?',
+                        style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffD30C7B)),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                            child: Text(
+                          "Poznajcie się w błyskawicznym tempie! ⚡\n\n🌟 Kręcicie kołem fortuny\n🌟 \"Kto teraz?\"\n🌟 Wylosowana osoba odpowiada na pytanie",
+                          style: TextStyle(fontSize: 24),
+                        )),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20, bottom: 5),
+                          child: Text(
+                            "Developerzy",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xffD30C7B)),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          "Bartosz Kępka",
+                          style: TextStyle(fontSize: 28),
+                        ),
+                        Text(
+                          "Łukasz Blachnicki",
+                          style: TextStyle(fontSize: 28),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-                SizedBox(height: 20),
                 Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20, bottom: 5),
-                        child: Text(
-                          "Developerzy",
-                          style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold),
-                        ),
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 5),
+                      child: Text(
+                        "Wsparcie",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffD30C7B)),
                       ),
-                      Text(
-                        "Bartosz Kępka",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                      Text(
-                        "Łukasz Blachnicki",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20, bottom: 5),
-                        child: Text(
-                          "Wsparcie",
-                          style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Text(
-                        "Zuzanna Olasek",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                      Text(
-                        "Oliwia Olasek",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                      Text(
-                        "Wiktoria Góralczyk",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                      Text(
-                        "Monika Łodzińska",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                      Text(
-                        "Sebastian Kula",
-                        style: TextStyle(fontSize: 28),
-                      ),
-                    ])
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Zuzanna Olasek",
+                      style: TextStyle(fontSize: 28),
+                    ),
+                    Text(
+                      "Oliwia Olasek",
+                      style: TextStyle(fontSize: 28),
+                    ),
+                    Text(
+                      "Wiktoria Góralczyk",
+                      style: TextStyle(fontSize: 28),
+                    ),
+                    Text(
+                      "Monika Łodzińska",
+                      style: TextStyle(fontSize: 28),
+                    ),
+                    Text(
+                      "Sebastian Kula",
+                      style: TextStyle(fontSize: 28),
+                    ),
+                  ],
+                )
               ],
             )),
       ],
