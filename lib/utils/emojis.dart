09f07a9cf3a35a730812c89e93893791b7ab1d13
Bartosz Kępka <bartoszkepka1999@gmail.com>
2023-11-06 15:16:34 +0100

    Fix Android files, upgrade flutter and packages, add required android… (#16)
    * Fix Android files, upgrade flutter and packages, add required android API level
    
    * Fix icon path

--- lib/utils/emojis.dart
+++ lib/utils/emojis.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class Emojis {
   static getTransformedEmoji(Image emoji) {
--- lib/utils/winner.model.dart
+++ lib/utils/winner.model.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../utils/players.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Winner extends Player {
   Winner({required int id, required Color color, required Image emoji})
